e4a86b5c8603b5d82aaf7ef15a1731d032b5373e
LahlouMohamed <lahloumohamed0901@gmail.com>
2019-11-23 21:34:51 +0100

    initialisation Spring boot

---
+++ src/test/java/com/emsi/pfaProject/PfaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.pfaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
