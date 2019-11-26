492ff0294d05de6a3c501baf4b4c201417267b91
LahlouMohamed <lahloumohamed0901@gmail.com>
2019-11-26 22:24:19 +0100

    (temporary) disable spring security

--- src/main/java/com/emsi/pfaProject/PfaProjectApplication.java
+++ src/main/java/com/emsi/pfaProject/PfaProjectApplication.java
@@ -3,7 +3,9 @@ package com.emsi.pfaProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+)
 public class PfaProjectApplication {
 
 	public static void main(String[] args) {
