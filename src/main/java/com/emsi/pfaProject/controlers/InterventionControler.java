d2785b9a28d1ac164352ebfbd92cfde05d4e4415
arouay <arouayabdelalim@gmail.com>
2019-11-26 02:45:51 +0100

    trying to solve Cors limitation [unsolved]

--- src/main/java/com/emsi/pfaProject/controlers/InterventionControler.java
+++ src/main/java/com/emsi/pfaProject/controlers/InterventionControler.java
@@ -1,16 +1,26 @@
 package com.emsi.pfaProject.controlers;
 
+import java.io.Serializable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emsi.pfaProject.entities.Intervention;
 import com.emsi.pfaProject.repositories.IInterventionRepository;
 
 @RestController
 @RequestMapping(value = "/interventions/")
 @CrossOrigin(origins = "http://localhost:4200")
-public class InterventionControler {
+public class InterventionControler implements Serializable{
 	@Autowired
 	private IInterventionRepository interventionRepository;
+	
+	@PostMapping(value="new")
+	public Intervention newIntervention(@RequestBody Intervention intervention) {
+		return interventionRepository.save(intervention);
+	}
 }
