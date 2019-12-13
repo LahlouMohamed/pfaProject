a3fc34fbabd444f9ce0b127dab83d1930f8be5fd
LahlouMohamed <lahloumohamed0901@gmail.com>
2019-12-13 14:13:14 +0100

    Ajout auto insertion Marque Modeles OnStart

--- src/main/java/com/emsi/pfaProject/PfaProjectApplication.java
+++ src/main/java/com/emsi/pfaProject/PfaProjectApplication.java
@@ -10,44 +10,48 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
 )
 public class PfaProjectApplication implements ApplicationRunner {
-	@Autowired
-	private IMarqueRepository iMarqueRepository;
-	@Autowired
-	private IModelRepository iModelRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(PfaProjectApplication.class, args);
-	}
+    @Autowired
+    private IMarqueRepository iMarqueRepository;
+    @Autowired
+    private IModelRepository iModelRepository;
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		this.insertMarquesModels();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PfaProjectApplication.class, args);
+    }
 
-	public void insertMarquesModels(){
-		Marque marque=new Marque();
-		Model model=new Model();
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // à décommenter pour auto insert Marque&Model de test (update application.properties)
+    	//this.insertMarquesModels();
+    }
 
-		//Marque & Model insert
-		marque.setDesignation("Peugeot");
-		iMarqueRepository.saveAndFlush(marque);
-		model.setMarque(marque);model.setDesignation("207");
-		model.setBoiteVitesse("M");model.setVersion("1.4HDI SPROTIUM");
-		model.setPuissFiscal(7);model.setEnergie("Diesel");
-		iModelRepository.saveAndFlush(model);
-		//Marque & Model insert
-		marque=new Marque();
-		model=new Model();
-		//Marque & Model insert
-		marque.setDesignation("BMW");
-		iMarqueRepository.saveAndFlush(marque);
-		model.setMarque(marque);model.setDesignation("114");
-		model.setBoiteVitesse("A");model.setVersion("2.0");
-		model.setPuissFiscal(9);model.setEnergie("Diesel");
-		iModelRepository.saveAndFlush(model);
-		//Marque & Model insert
-	}
+    public void insertMarquesModels() {
+        Marque marque = new Marque();
+        Model model = new Model();
+        Random r = new Random();
+        //Marque & Model insert
+        for (int i = 1; i <= 8; i++) {
+            marque.setDesignation("Voiture" + i);
+            iMarqueRepository.saveAndFlush(marque);
+            for (int j = 1; j <= 3; j++) {
+                model.setMarque(marque);
+                model.setDesignation("Mod" + j+" V"+i);
+                model.setBoiteVitesse("Manuelle");
+                model.setVersion("1.4HDI SPROTIUM");
+                model.setPuissFiscal(r.nextInt(16) + 1);
+                model.setEnergie("Diesel");
+                iModelRepository.saveAndFlush(model);
+                model = new Model();
+            }
+            marque = new Marque();
+
+        }
+        //Marque & Model insert
+    }
 }
